c7161ac08dddaa4d49724ef38c184c4a41aa6cdf
Valentine Pylypchuk <strheadshot1997@gmail.com>
2024-01-19 14:44:20 +0200

    rename basename for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-08-phonebook">
+  <BrowserRouter basename="/React-phonebook">
   <Provider store={store}>
   <PersistGate persistor={persistor}>
     <App />
