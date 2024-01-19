0fdfd0ab808a7cc77d079dbe646cccf931ddea6d
Valentine Pylypchuk <strheadshot1997@gmail.com>
2024-01-19 14:37:35 +0200

    add strictMode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <BrowserRouter basename="/goit-react-hw-08-phonebook">
   <Provider store={store}>
   <PersistGate persistor={persistor}>
